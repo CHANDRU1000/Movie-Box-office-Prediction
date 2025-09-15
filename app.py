812a31f4460e73bd580dae43ef6fd4f0cd710250
CHANDRU1000 <149527690+CHANDRU1000@users.noreply.github.com>
2025-09-15 12:49:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,14 @@ import pickle
 
 st.set_page_config(page_title="Movie Box Office Prediction", layout="centered")
 
-# Load Linear Regression model & encoders
 with open("linear_movie_model.pkl", "rb") as f:
     model, le_genre, le_month = pickle.load(f)
 
-# App title
 st.title("🍿🎬 Movie Box Office Prediction App")
 st.write("Predict the box office revenue of a movie before release using **Linear Regression**!")
 
 st.sidebar.header("Enter Movie Details:")
 
-# Inputs
 genre = st.sidebar.selectbox("Genre", le_genre.classes_)
 budget = st.sidebar.number_input("Budget (in Crores)", min_value=1, max_value=1000, value=50)
 cast_popularity = st.sidebar.slider("Cast Popularity (1-10)", 1.0, 10.0, 5.0)
@@ -22,7 +19,6 @@ director_popularity = st.sidebar.slider("Director Popularity (1-10)", 1.0, 10.0,
 marketing_spend = st.sidebar.number_input("Marketing Spend (in Crores)", min_value=0, max_value=500, value=20)
 release_month = st.sidebar.selectbox("Release Month", le_month.classes_)
 
-# Prediction button
 if st.sidebar.button("Predict Revenue"):
     input_data = pd.DataFrame({
         "Genre": [le_genre.transform([genre])[0]],
